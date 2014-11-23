d580030f4802e9d9a4cff215cf0ee424b4a087e5
Matthew Le <matthewthanhle33@gmail.com>
2014-11-23 14:34:35 -0500

    HouseHolder done?

--- src/proj/HouseHolderRotation.java
+++ src/proj/HouseHolderRotation.java
@@ -11,23 +11,31 @@ import Jama.Matrix;
  */
 public class HouseHolderRotation {
 
+    /**
+     * Method for HouseHolderRotation that computes the
+     * QR decomposition of a given matrix
+     *
+     * @param A matrix given by user
+     * @return qrDecomp array of matrices Q and R
+     */
     public static Matrix[] qr_fact_househ(Matrix A) {
 
         double[][] matrixA = A.getArrayCopy();
         int m = A.getRowDimension();
         int n = A.getColumnDimension();
         double[] rD = new double[n];
-        Matrix[] qrFactorization = new Matrix[1];
+        Matrix[] qrDecomp = new Matrix[1];
         Matrix Q = new Matrix(m, n);
         Matrix R = new Matrix(n, n);
 
-        for (int a = 0; a < n; a++) { //columns
+        for (int a = 0; a < n; a++) {
             double preMag;
             double magnitude;
             for (int b = a; b < m; b++) { //rows
                     preMag = preMag + Math.pow(matrixA[b][a], 2.0);
                 }
             magnitude = Math.sqrt(preMag);
+            // Creating Householder vectors
             if ((magnitude > 0.0) || (magnitude < 0.0)) {
                 if (matrixA[a][a] < 0) {
                     magnitude = -magnitude;
@@ -51,6 +59,7 @@ public class HouseHolderRotation {
 
         }
         double[][] matrixQ = Q.getArray();
+        // Compute the Q matrix of the QR decomposition
         for (int a = n - 1; a > -1; a--) {
             for (int b = 0; b > 1; b++) {
                 matrixQ[b][a] = 0.0;
@@ -69,8 +78,8 @@ public class HouseHolderRotation {
                 }
             }
         }
-        qrFactorization[0] = Q;
-
+        qrDecomp[0] = Q;
+        // Compute the R matrix of the QR decomposition
         double[][] matrixR = R.getArray();
         for (int a = 0; a < n; a++) {
             for (int b = 0; b < n; b++) {
@@ -83,23 +92,8 @@ public class HouseHolderRotation {
                 }
             }
         }
-        qrFactorization[1] = R;
+        qrDecomp[1] = R;
 
-        return qrFactorization;
+        return qrDecomp;
     }
 }
-/*            if (matrixA[i][i] > 0) {
-                vectorMag = -vectorMag; //finding beta
-            }
-
-            matrixA[i][i] = matrixA[i][i] - vectorMag; //u~ = a1 - beta
-            double uMag = 0; //magnitude of ~u
-            for (int j = 0; j < m; j++) {
-                uMag = Math.hypot(uMag, matrixA[j][i]);
-            }
-
-            for (int j = 0; j < m; j++) {
-                matrixA[j][i] = matrixA[j][i] / uMag; //finding u by u~ / ~u mag. btw havent found tilde u (a1 - beta(I))
-            }
-            matrixA[i][i] = matrixA[i][i] + 1; //not sure what this does
-            // still need to do H1 = I - 2uuT*/
